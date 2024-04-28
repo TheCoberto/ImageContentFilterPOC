06da02a312006ae6cd31b2eb6348392a61e793dc
wkelly <coby.kelly.ck@gmail.com>
2024-04-27 23:44:08 -0400

    default global strings

--- ImageContentFilterPOC/Globals.cs
+++ ImageContentFilterPOC/Globals.cs
@@ -8,12 +8,12 @@ namespace ImageContentFilterPOC
 {
     class Globals
     {
-        public const string ContentModSubscriptionKey = "d6d35bfeca6d404ea0628762b661f674";
-        public const string ContentModEndPoint = "https://content-mod.cognitiveservices.azure.com/";
+        public const string ContentModSubscriptionKey = "your key";
+        public const string ContentModEndPoint = "your endpoint";
 
-        public const string FormRecognizerSubscriptionKey = "bbd92dac3c7c4b35ab644e7b1f880ff0";
-        public const string FormRecognizerEndPoint = "https://frec.cognitiveservices.azure.com/";
+        public const string FormRecognizerSubscriptionKey = "your key";
+        public const string FormRecognizerEndPoint = "your endpoint";
 
-        public const string TOP_DIR = @"C:\Users\wkelly\source\repos\Edm.Sandbox\ImageContentFilterPOC\";
+        public const string TOP_DIR = @"your file path";
     }
 }
