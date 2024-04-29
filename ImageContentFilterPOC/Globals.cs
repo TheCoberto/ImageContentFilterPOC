facfc5fe398848314c27a0f92d4caa09e7f84c98
wkelly <coby.kelly.ck@gmail.com>
2024-04-28 22:40:12 -0400

    Fix globals part 3

--- ImageContentFilterPOC/Globals.cs
+++ ImageContentFilterPOC/Globals.cs
@@ -7,12 +7,14 @@ using System.Threading.Tasks;
 namespace ImageContentFilterPOC
 {
     public class Globals
-    class Globals
     {
         public const string ContentModSubscriptionKey = "your key";
         public const string ContentModEndPoint = "your endpoint";
 
-        public const string TOP_DIR = @"C:\Repos\Content Moderation POC";
+        public const string FormRecognizerSubscriptionKey = "your key";
+        public const string FormRecognizerEndPoint = "your endpoint";
+
+        public const string TOP_DIR = @"your project directory file path";
 
         public const string RESULT_PLACEHOLDER = "Select an image file(.jpg, .jpeg, .png, .gif)";
         public const string RESULT_EVALUATING = "Evaluating...";
