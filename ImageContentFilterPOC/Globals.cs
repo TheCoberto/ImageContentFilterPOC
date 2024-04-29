5bbe2a5cd8b0bb812facb54fc5198db6f78f394d
Coby Kelly <coby.kelly.ck@gmail.com>
2024-04-28 22:32:38 -0400

    fix globals

--- ImageContentFilterPOC/Globals.cs
+++ ImageContentFilterPOC/Globals.cs
@@ -15,6 +15,7 @@ namespace ImageContentFilterPOC
         public const string FormRecognizerSubscriptionKey = "your key";
         public const string FormRecognizerEndPoint = "your endpoint";
 
-        public const string TOP_DIR = @"your project directory file path";
+        public const string RESULT_PLACEHOLDER = "Select an image file(.jpg, .jpeg, .png, .gif)";
+        public const string RESULT_EVALUATING = "Evaluating...";
     }
 }
